a433e333f9331934e6fcc910243ad9f66c3944f2
unknown <Sanjay.Singh@ATMECSINLT-011.ATMECS.CORP>
2016-08-31 20:26:50 +0530

    Done: React Lifecycle - author names listed. Done: Demo Dynamic Data and Lifecycle

---
+++ src/components/authors/authorPage.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+
+var Authors = React.createClass({
+    getInitialState: function() {
+        return {
+            authors: []
+        };
+    },
+
+    componentWillMount: function() {
+        this.setState({ authors: AuthorApi.getAllAuthors()} );
+    },
+
+    render: function() {
+        var createAuthorRow = function(author) {
+            return (
+                <div>
+                    <tr key={author.id}>
+                        <td>
+                            <a href={"/#authors" + author.id}>{author.id}</a>
+                        </td>
+                        <td>
+                            {author.firstName} {author.lastName}
+                        </td>
+                    </tr>
+                </div>
+            );
+        };
+
+        return (
+            <div>
+                <h1>Authors</h1>
+
+                <table className="table table-hover">
+                    <thead>
+                        <th>ID</th>
+                        <th>Name</th>
+                    </thead>
+                    <tbody>
+                        {this.state.authors.map(createAuthorRow, this)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+});
+
+module.exports = Authors;
