301caa552140fd25ffc893140e5cca9d02124c52
sanjaysi <cloudwithpython.com>
2016-09-02 00:55:48 +0530

    React Forms: Consuming Reusable Inputs

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -1,30 +1,25 @@
 "use strict";
 
 var React = require('react');
+var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
 	render: function() {
 		return (
 			<form>
 				<h1>Manage Author</h1>
-				<label htmlFor="firstName">First Name</label>
-				<input type="text"
-					name="firstName"
-					className="form-control"
-					placeholder="First Name"
-					ref="firstName"
-					onChange={this.props.onChange}
-					value={this.props.author.firstName} />
-				<br />
 
-				<input type="text"
+				<Input 
+					name="firstName"
+					label="First Name"
+					value={this.props.author.firstName}
+					onChange={this.props.onChange} />
+
+				<Input 
 					name="lastName"
-					className="form-control"
-					placeholder="Last Name"
-					ref="lastName"
-					onChange={this.props.onChange}
-					value={this.props.author.lastName} />
-				<br />
+					label="Last Name"
+					value={this.props.author.lastName}
+					onChange={this.props.onChange} />
 
 				<input type="submit" value="Save" className="btn btn-default" />
 			</form>
